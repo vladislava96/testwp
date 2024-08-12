042c554f5e214ac23878f49ef8b46c3b4e42dd10
Vladislava <vladislavaskrabova@gmail.com>
2024-08-12 15:52:07 +0300

    fix: delete transition for share overlay

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const shareOverlay = document.querySelector(".share-overlay");
 
 shareOverlayLink.addEventListener("mouseover", () => {
   shareOverlay.style.display = "flex";
-  shareOverlay.style.opacity = "1";
 })
 shareOverlayLink.addEventListener("mouseout", () => {
   shareOverlay.style.display = "none";
-  shareOverlay.style.opacity = "0";
 })
